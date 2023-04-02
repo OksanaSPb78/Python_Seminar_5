0c5b7d7ae268775175019bcb67e6d566fa9c6fa5
OksanaSPb78 <121289171+OksanaSPb78@users.noreply.github.com>
2023-04-02 17:37:24 +0300

    uptate homework6 second times

--- TasksHomeWork6.py
+++ TasksHomeWork6.py
@@ -24,12 +24,20 @@
 import random
 list_1 = [random.randint(-5, 20) for _ in range(20)]
 print(list_1)
-list_new = []
 min_n = int(input('Задайте минимум: '))
 max_n = int(input('Задайте максимум: '))
-if max_n >= min_n:
-    for item in range(len(list_1)):
-        if max_n >= list_1[item] and min_n <= list_1[item]:
-           list_new.append(item)
-    print(item)
+for i in range(len(list_1)):
+    if max_n >= list_1[i] and min_n <= list_1[i]:
+        #    list_new.append(item)
+        print(i)
 
+
+
+# # ЭТАЛОННОЕ РЕШЕНИЕ
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# print(list_1)
+# min_number = int(input('Input min '))
+# max_number = int(input('Input max '))
+# for i in range(len(list_1)):
+#     if min_number <= list_1[i] <= max_number:
+#         print(f'Указанному диапазону от {min_number} до {max_number} соответствуют индексы {i}')
